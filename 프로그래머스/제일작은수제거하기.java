49f54ff5227c1c13a627aac239209cc7208a1ff3
Hyunwoo <320woo@naver.com>
2021-12-20 23:56:08 +0900

    2021

---
+++ 프로그래머스/제일작은수제거하기.java
@@ -0,0 +1,39 @@
+package Programmers;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int min = 0;
+        int[] copy = arr.clone();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        if(arr.length == 1) {
+            answer = new int[1];
+            answer[0] = -1;
+        }
+        else {
+            answer = new int[arr.length];
+            Arrays.sort(copy);
+            min = copy[0];
+            int k = arr.length;
+
+            for(int i=0; i<arr.length; i++){
+                if(arr[i] == min){
+                    k--;
+                    continue;
+                }
+                list.add(arr[i]);
+            }
+            
+            answer = new int[k];
+            
+            for(int i=0; i<k; i++){
+                answer[i] = list.get(i);
+            }
+            
+        }
+        
+        return answer;
+    }
+}
